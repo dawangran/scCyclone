2dc66e7ac1527ef0f3924870b374e1151e957bf0
dawangran <605547565@qq.com>
2024-10-12 14:15:39 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables'] 
+extensions = [] 
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,9 +28,5 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 
-
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
