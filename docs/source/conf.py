e91c68fc84e6b9532fa427a164ddc5fec33e2cab
dawangran <605547565@qq.com>
2024-10-12 14:22:35 +0800

    change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+
 project = 'scCyclone'
 copyright = '2024, Dawn'
 author = 'Dawn'
@@ -14,7 +21,8 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [] 
+extensions = ['recommonmark','sphinx_markdown_tables'] 
+
 
 templates_path = ['_templates']
 exclude_patterns = []
