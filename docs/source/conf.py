ec4984d2656c7e326d905c3b87ce2fa15e4fa719
dawangran <605547565@qq.com>
2024-10-12 14:31:01 +0800

    add

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 
-# import sphinx_rtd_theme
-# html_theme = "sphinx_rtd_theme"
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 
@@ -21,7 +21,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [] 
+extensions = ['recommonmark','sphinx_markdown_tables'] 
 
 
 templates_path = ['_templates']
